20a802312bb6e6f7665a9c9306f97e7aede2e88d
Tom Fischer <tgfischer6@gmail.com>
2018-07-05 22:15:09 -0400

    Added routes for signing in and resetting the temporary password

--- src/routes/signIn.js
+++ src/routes/signIn.js
@@ -1,10 +1,41 @@
 const router = require("express").Router({ mergeParams: true });
 const HttpStatus = require("http-status-codes");
+const asyncHandler = require("express-async-handler");
 const middleware = require("tom-fischer-middleware");
 const joi = require("tom-fischer-joi");
 
-const signIn = (req, res) => res.status(HttpStatus.OK).json(res.locals.body);
+const UserService = require("../services/UserService");
 
-router.post("/", middleware.validate.body(joi.signIn.signInSchema), signIn);
+const signIn = async (req, res) => {
+  const { email, password } = res.locals.body;
+  const userService = new UserService();
+
+  const result = await userService.initiateAuth({ email, password });
+  res.status(HttpStatus.OK).json(result);
+};
+
+const resetTemporaryPassword = async (req, res) => {
+  const { email, password, session } = res.locals.body;
+  const userService = new UserService();
+
+  const result = await userService.resetTemporaryPassword({
+    email,
+    password,
+    session
+  });
+  res.status(HttpStatus.OK).json(result);
+};
+
+router.post(
+  "/",
+  middleware.validate.body(joi.signIn.signInSchema),
+  asyncHandler(signIn)
+);
+
+router.post(
+  "/reset-temporary-password",
+  middleware.validate.body(joi.signIn.resetTemporaryPasswordSchema),
+  asyncHandler(resetTemporaryPassword)
+);
 
 module.exports = router;
